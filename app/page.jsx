de2c46fcfa11a0f42f8a1a7e6d2abc62e1a4b222
Solomon Lee <solomonlee@dhcp-vl2041-30990.eduroam.cornell.edu>
2026-04-10 17:25:51 -0400

    add photos

--- app/page.jsx
+++ app/page.jsx
@@ -53,6 +53,7 @@ const LIFE_PHOTOS = [
   { src: "/life/1D2108C3-270B-47A6-9D25-3B3FA9F82D77_1_105_c.jpeg" , caption: "Happy Anniversary Nat!"},
   { src: "/life/5806356D-AC0D-4784-93EF-E08348686511_1_105_c.jpeg", caption: "Wish I could bring this to the US :(" },
   { src: "/life/68BDCE2A-B219-462C-9455-03A230CC32EB_1_105_c.jpeg", caption: "Dressing up in the Forbidden City!"}
+  { src: "/life/54BF910D-6140-4487-89F6-63849F4144FD_1_105_c.jpeg", caption: "Summer Palace Vibes!"}
 ];
 
 const CORNELL = {
